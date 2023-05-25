d4a06a2378cbdf73c6e04f2652c397978b69f00c
ivanmolero <imolerod@gmail.com>
2023-05-24 20:37:19 -0500

    ejercicio 12 finalizado

--- src/demoegg/S02Ejercicio12.java
+++ src/demoegg/S02Ejercicio12.java
@@ -5,6 +5,8 @@
  */
 package demoegg;
 
+import java.util.Scanner;
+
 /**
  *
  * @author imolero
@@ -16,6 +18,22 @@ public class S02Ejercicio12 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner scan = new Scanner(System.in);
+        int num1, num2;
+        
+        System.out.println("ingrese el primer número: ");
+        num1 = scan.nextInt();
+        System.out.println("ingrese el segundo número: ");
+        num2 = scan.nextInt();
+        esMultiplo(num1, num2);
+    }
+    
+    public static void esMultiplo(int num1, int num2) {
+        if (num1 % num2 == 0) {
+            System.out.println("el primer número (" + num1 + ") es múltiplo del segundo (" + num2 + ")");
+        } else {
+            System.out.println("el primer número no es múltiño del segundo.");
+        }
     }
     
 }
